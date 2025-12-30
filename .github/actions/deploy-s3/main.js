e967faf110daf1430a04951dd2b1c5282b6ef136
rickylim <rlim.email@gmail.com>
2025-12-30 14:24:54 +0100

    Add output for deployed URL in S3 deployment action and update workflow to display it

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -6,9 +6,13 @@ function run() {
     const bucket = core.getInput("bucket", { required: true });
     const region = core.getInput("region", { required: true });
     const distFolder = core.getInput("dist-folder", { required: true });
+    const deployedUrl = `https://${bucket}.s3.${region}.amazonaws.com/`;
 
     core.notice("Starting deployment to S3...");
     exec.exec(`echo aws s3 sync ${distFolder} s3://${bucket} --region ${region} --delete`)
+
+    core.setOutput("deployed-url", deployedUrl);
+    core.notice(`Deployment completed! Application is available at: ${deployedUrl}`);
 }
 
 run();
