6851cf711d6661739e498f96195857eb8bc63908
rickylim <rlim.email@gmail.com>
2025-12-30 14:12:36 +0100

    Add S3 deployment action and update workflow to use it

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -3,7 +3,12 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+    const bucket = core.getInput("bucket", { required: true });
+    const region = core.getInput("region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
     core.notice("Starting deployment to S3...");
+    exec.exec(`echo aws s3 sync ${distFolder} s3://${bucket} --region ${region} --delete`)
 }
 
 run();
